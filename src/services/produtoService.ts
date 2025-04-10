e8dcf0292ec2aeb8bce172c5a366e7b4ab5515d9
ThamiresAntunes <antunesthamires8@gmail.com>
2025-04-09 21:50:13 -0300

    feature: criando o servico de criar produtos

---
+++ src/services/produtoService.ts
@@ -0,0 +1,20 @@
+import { Produto } from "../models/Produto";
+import { prisma } from "../config/cliente-prisma";
+
+export const criarProduto = async (data: Produto) => {
+
+    try{
+        return await prisma.produto.create({
+            data: {
+                nome: data.nome,
+                preco: data.preco,
+            }
+        })
+    }
+    catch(error){
+        console.log(error);
+        return null;
+    }
+    
+}
+
