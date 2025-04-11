bdd44704b9d67e2c96cdbd1030f5efd29000a839
ThamiresAntunes <antunesthamires8@gmail.com>
2025-04-10 21:02:02 -0300

    feature: criando o controller de listar os produtos

--- src/controllers/produtoController.ts
+++ src/controllers/produtoController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { criarProduto } from "../services/produtoService";
+import { criarProduto, listarProdutos } from "../services/produtoService";
 
 export const criar = async (requisicao: Request, resposta: Response) => {
     //produto armazena os dados de um produto que foi passado no body da requisicao
@@ -11,3 +11,15 @@ export const criar = async (requisicao: Request, resposta: Response) => {
     resposta.status(500).json({erro: "Erro ao salvar produto"});
     return;
 }
+
+export const listar = async (requisicao: Request, resposta: Response) => {
+    const lista = await listarProdutos();
+    if(lista !== null){
+        resposta.status(200).json(lista);
+        return;
+    }
+    resposta.status(404).json({erro: "lista de produtos esta vazia"});
+    return;
+}
+
+
