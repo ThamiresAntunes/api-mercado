46cf0af1f838431b961b4f40851c0e9dda66fd11
ThamiresAntunes <antunesthamires8@gmail.com>
2025-04-10 23:05:06 -0300

    feature: criando a router de atualizar o produto

--- src/routes/produtoRoutes.ts
+++ src/routes/produtoRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { criar, listar } from "../controllers/produtoController";
+import { criar, listar, atualizar } from "../controllers/produtoController";
 
 const routerProduto = Router();
 
 routerProduto.post('/criarProduto', criar);
 routerProduto.get('/listarProdutos', listar);
+routerProduto.put('/atualizarProduto/:codigo', atualizar);
 
 
 export default routerProduto;
