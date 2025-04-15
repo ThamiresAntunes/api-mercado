d826f1ae473a00c71631265f38ebe8c627a16471
ThamiresAntunes <antunesthamires8@gmail.com>
2025-04-15 20:53:39 -0300

    feature: criando a router para ver um produto

--- src/routes/produtoRoutes.ts
+++ src/routes/produtoRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { criar, listar, atualizar, deletar } from "../controllers/produtoController";
+import { criar, listar, atualizar, deletar, listarIndividual } from "../controllers/produtoController";
 
 const routerProduto = Router();
 
 routerProduto.post('/criarProduto', criar);
 routerProduto.get('/listarProdutos', listar);
+routerProduto.patch('/listarIndividual/:codigo', listarIndividual);
 routerProduto.put('/atualizarProduto/:codigo', atualizar);
 routerProduto.delete('/deletarProduto/:codigo', deletar);
 
