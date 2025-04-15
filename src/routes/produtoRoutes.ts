c9d5f056b70d397ce4fcae4871689ee8b88aca24
ThamiresAntunes <antunesthamires8@gmail.com>
2025-04-15 19:26:15 -0300

    feature: criando a router de deletar produto

--- src/routes/produtoRoutes.ts
+++ src/routes/produtoRoutes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { criar, listar, atualizar } from "../controllers/produtoController";
+import { criar, listar, atualizar, deletar } from "../controllers/produtoController";
 
 const routerProduto = Router();
 
 routerProduto.post('/criarProduto', criar);
 routerProduto.get('/listarProdutos', listar);
 routerProduto.put('/atualizarProduto/:codigo', atualizar);
-
+routerProduto.delete('/deletarProduto/:codigo', deletar);
 
 export default routerProduto;
