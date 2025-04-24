b1795564e81d894d4dd52960e0ab0d30a15fe40e
ThamiresAntunes <antunesthamires8@gmail.com>
2025-04-23 21:51:39 -0300

    feature: adicionando o middleware de verificar campos na router de produto

--- src/routes/produtoRoutes.ts
+++ src/routes/produtoRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { criar, listar, atualizar, deletar, listarIndividual } from "../controllers/produtoController";
+import { verificarCampo } from "../middlewares/verificarCampos";
 
 const routerProduto = Router();
 
-routerProduto.post('/criarProduto', criar);
+routerProduto.post('/criarProduto', verificarCampo, criar);
 routerProduto.get('/listarProdutos', listar);
 routerProduto.patch('/listarIndividual/:codigo', listarIndividual);
 routerProduto.put('/atualizarProduto/:codigo', atualizar);
